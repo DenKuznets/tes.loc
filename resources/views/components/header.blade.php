26fe144c3d0ba93a37c26181399cd0614acfd933
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-05-05 15:31:50 +0300

    header done

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header class="fixedHeader">
     <div class="header ">
-        <div class="header__content containers">
+        <div class="containers header__content">
             <div class="header__logo">
                 <a
                  {{-- href="{{ route('index') }}" --}}
@@ -11,13 +11,12 @@
             <div class="header__nav">
                 <ul class="nav nav__list-header">
                     <li class="nav__item-header"><a href="#services">Услуги</a></li>
-                    <li class="nav__item-header"><a href="#products">Продукция</a></li>
-                    <li class="nav__item-header"><a href="#about">О компании</a></li>
-                    <li class="nav__item-header"><a href="#objects">Объекты</a></li>
+                    <li class="nav__item-header"><a href="#products">Выполненные работы</a></li>
+                    <li class="nav__item-header"><a href="#about">Отзывы</a></li>
                     <li class="nav__item-header"><a href="#contacts">Контакты</a></li>
                 </ul>
             </div>
-            <a class="header__phone" href="tel:+79219519260">+7 921 951-92-60</a>
+            <a class="header__phone" href="tel:+79157556322">+7 915 755-63-22</a>
         </div>
     </div>
 </header>
