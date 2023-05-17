3353af996e162cec5b5187d8b57f7fe199a36ad0
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-05-17 16:16:25 +0300

    contacts started

--- resources/views/sections/contacts.blade.php
+++ resources/views/sections/contacts.blade.php
@@ -4,33 +4,34 @@
         <div class="contacts__text">
             <div class="contacts__text-contacts">
                 <h3>Наши контакты</h3>
-                <p href="">
-                    <img class="svg" src="{{ asset('assets/img/location-icon.svg') }}" alt="">
-                    Тульская обл., г. Ефремов, ул. Свердлова, д. 67
-                </p>
-                <a href="mailto:tulenergoservis@yandex.ru">
-                    <img class="svg" src="{{ asset('assets/img/mail-icon.svg') }}" alt="">
-                    tulenergoservis@yandex.ru
-                </a>
                 <a class="header__top-phone" href="tel:+74874165666">
-                    <img class="svg" src="{{ asset('assets/img/phone-icon.svg') }}" alt="">
+                    <img class="svg" src="{{ asset('assets/img/phone-icon-white.svg') }}" alt="">
                     8(48741)6–56–66
                 </a>
+                <a href="mailto:tulenergoservis@yandex.ru">
+                    <img class="svg" src="{{ asset('assets/img/mail-icon-white.svg') }}" alt="">
+                    tulenergoservis@yandex.ru
+                </a>
+                <p href="">
+                    <img class="svg" src="{{ asset('assets/img/location-icon-white.svg') }}" alt="">
+                    Тульская обл., г. Ефремов, ул. Свердлова, д. 67
+                </p>
             </div>
             <div class="contacts__text-form">
                 <h3 class="modal-title">Напишите нам</h3>
-                <p class="modal-text">Оставьте свой номер телефона, и мы поможем воплотить вашу мечту в реальность</p>
                 <form class="modal-form">
                     <input class="modal-form-input" type="text" name="name" placeholder="Имя" id="inputName"
                         required>
                     <input class="modal-form-input" type="text" name="phone" placeholder="Телефон" id="inputPhone"
                         required>
+                    <textarea name="text" id="inputComments" placeholder="Опишите ваши пожелания"></textarea>
+
                     <div class="modal-checkbox">
                         <input type="checkbox" id="checkbox">
                         <label>Согласен на обработку персональный данных</label>
                     </div>
                     <div class="modal-button">
-                        <button class="btn btn-cta" id="sendMail" disabled>Оставить заявку</button>
+                        <button class="btn" id="sendMail" disabled>Оставить заявку</button>
                     </div>
                 </form>
             </div>
