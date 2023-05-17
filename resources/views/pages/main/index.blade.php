d3903f65c8840fb65f0d2fc23d5593445b3853d0
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-05-17 08:06:54 +0300

    anchor underline

--- resources/views/pages/main/index.blade.php
+++ resources/views/pages/main/index.blade.php
@@ -7,10 +7,10 @@
 @section('keywords', 'Заполни ключевые слова')
 @section('content')
     <main class="main">
-        @include('../sections.hero')
+        {{-- @include('../sections.hero') --}}
+        @include('../sections.services')
         {{-- @include('../sections.advantages')
         @include('../sections.about')
-        @include('../sections.services')
         @include('../sections.works')
         @include('../sections.reviews')
         @include('../sections.contacts') --}}
